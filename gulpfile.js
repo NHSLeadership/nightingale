45ef7c4c7b9de421ea5a2b04a88e6ecf773b138a
Christopher Mills <chris.mills@leadershipacademy.nhs.uk>
2017-03-14 10:25:47 +0000

    [refs #00001] Adding basic Gulp and SASS features for inital development
    Inital commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp'),
+    args = require('yargs').argv,
+    path = require('path'),
+    $ = require('gulp-load-plugins')({
+        pattern: ['gulp-*', 'gulp.*', 'autoprefixer', 'path']
+    });
+
+const basePaths = {
+};
+
+const paths = {
+    sass: {
+        src: `*.scss`,
+        dest: `.`,
+        watch: `**/*.scss`,
+        lintFiles: `**/*.scss`,
+        autoprefixerBrowsers: [`last 3 version`]
+    }
+};
+
+gulp.task('sass', () => {
+    return gulp.src(paths.sass.src)
+        .pipe($.sass({
+            outputStyle: 'expanded'
+        }).on('error', $.sass.logError))
+        .pipe($.postcss([
+            $.autoprefixer({
+                browsers: paths.sass.autoprefixerBrowsers
+            }),
+        ]))
+        .pipe($.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.sass.dest))
+});
+
+gulp.task('default', ['sass']);
