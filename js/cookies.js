1cb245d39b78361c88a338896f1b9844c35aa050
Android63 <ajblane@gmail.com>
2017-11-14 16:08:18 +0000

    [refs #00104] Use localStorage for cookie ribbon
    Store user's decision to close cookie ribbon in browser's local
    storage area - and check this before displaying ribbon.

--- js/cookies.js
+++ js/cookies.js
@@ -4,8 +4,16 @@
   var cookieRibbon = document.getElementById('jsCookieRibbon');
   var cookieBtn = document.getElementById('jsCookieBtn');
 
+  // Don't show ribbon if cookie permission granted
+  if(localStorage.getItem("cookie-enable")=="1"){
+    cookieRibbon.parentNode.removeChild(cookieRibbon);
+  }
+
+  // Close cookie ribbon and store decision in local storage
   cookieBtn.addEventListener('click', function () {
     cookieRibbon.parentNode.removeChild(cookieRibbon);
+    // Store cookie permission
+    localStorage.setItem("cookie-enable", "1");
   });
 
 }());
