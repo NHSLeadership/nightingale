d7ed5ecab83a5e217cfb8201835802de1eaafe29
Android63 <ajblane@gmail.com>
2018-01-30 13:17:33 +0000

    [refs #00187] Check elements exist
    Check jsCookieRibbon and jsCookieBtn exist before using them.

--- js/cookies.js
+++ js/cookies.js
@@ -1,19 +1,26 @@
 // Cookies Ribbon
 (function(){
 
-  var cookieRibbon = document.getElementById('jsCookieRibbon');
-  var cookieBtn = document.getElementById('jsCookieBtn');
+  // Check jsCookieRibbon exists before using it
+  if (document.getElementById('jsCookieRibbon')) {
+    var cookieRibbon = document.getElementById('jsCookieRibbon');
 
-  // Don't show ribbon if cookie permission granted
-  if(localStorage.getItem("cookie-enable")=="1"){
-    cookieRibbon.parentNode.removeChild(cookieRibbon);
+    // Check jsCookieBtn exists before using it
+    if (document.getElementById('jsCookieBtn')) {
+      var cookieBtn = document.getElementById('jsCookieBtn');
+
+      // Don't show ribbon if cookie permission granted
+      if(localStorage.getItem("cookie-enable")=="1"){
+        cookieRibbon.parentNode.removeChild(cookieRibbon);
+      }
+
+      // Close cookie ribbon and store decision in local storage
+      cookieBtn.addEventListener('click', function () {
+        cookieRibbon.parentNode.removeChild(cookieRibbon);
+        // Store cookie permission
+        localStorage.setItem("cookie-enable", "1");
+      });
+    }
   }
-
-  // Close cookie ribbon and store decision in local storage
-  cookieBtn.addEventListener('click', function () {
-    cookieRibbon.parentNode.removeChild(cookieRibbon);
-    // Store cookie permission
-    localStorage.setItem("cookie-enable", "1");
-  });
 
 }());
